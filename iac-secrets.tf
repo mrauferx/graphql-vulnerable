26835be07aaeed5f2c5e23b6c36146a798decfdf
mraufer <49677050+mraufer@users.noreply.github.com>
2025-01-15 14:27:55 +0100

    Update iac-secrets.tf

--- iac-secrets.tf
+++ iac-secrets.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "ASIAZG5Y3JXC5UQKAJ8Y"
-  secret_key = "KsOuKcmeGH95O1zIEFP9TOjhy03yfxreF8gDi7+B"
+  access_key = $tf-access
+  secret_key = $tf-secret
   region     = "us-west-2"
 }
 
